532f67962996652d05a651b6678d50ac2384fea6
Nawaz Chowdhury <Nawaz.Chowdhury@trios.com>
2024-06-06 14:24:11 -0400

    v7

---
+++ 1.py
@@ -0,0 +1,7 @@
+a="John"
+print ("Hello My Name is",a)
+
+_Hello="1"
+print ("Hello My Name is",_Hello)
+
+
---
+++ 2.py
@@ -0,0 +1,11 @@
+a=[]
+a.append("Nawaz")
+a.append("Gurpreet")
+a.append("Harpreet")
+
+print("This is my stack",a)
+k=a.pop(0)
+print("This is Value",k)
+k=a.pop(0)
+print("This is Value",k)
+print("This is my stack",a)
